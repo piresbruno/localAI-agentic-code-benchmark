01a809a4e112f9e9778692762a3eb6ad85a076a5
Bruno Pires <bruno@blastersystems.com>
2026-09-02 04:13:01 +0100

    chore(benchmarks): purge executed run data from results and results-archive

--- results/fastcrc/DeepSeekV4Flash-Vision-EXP-ablit-pi-20260902-2/src/Fastcrc/Io.cs
+++
@@ -1,7 +0,0 @@
-namespace Fastcrc;
-
-/// The only module doing file I/O.
-public static class Io
-{
-    public static byte[] ReadAllBytes(string path) => File.ReadAllBytes(path);
-}
--- results/fastcrc/DeepSeekV4Flash-Vision-EXP-ablit-pi-20260902-2/src/Fastcrc/Program.cs
+++
@@ -1,6 +0,0 @@
-namespace Fastcrc;
-
-public static class Program
-{
-    public static int Main(string[] args) => Cli.RunCli(args);
-}
